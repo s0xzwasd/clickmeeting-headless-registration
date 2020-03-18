a72d664965982cbd0557f692cf93832cee8d3496
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-18 22:08:36 +0300

    feat: add basic func

--- registration.js
+++ registration.js
@@ -1,6 +1,11 @@
 const puppeteer = require("puppeteer");
+const generator = require("generate-password");
 
-function delay(time) {
+const args = process.argv.slice(2);
+const accountID = args[0];
+const companyName = args[1];
+
+const delay = (time) => {
   return new Promise(function(resolve) {
     setTimeout(resolve, time);
   });
@@ -9,20 +14,38 @@ function delay(time) {
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  const login = "tb-master+" + accountID + "@ya.ru";
+  const password = generator.generate({
+    length: 10,
+    numbers: true
+  });
 
-  await page.goto("https://stackoverflow.com/users/login");
-  await page.type("#email", "email");
-  await page.type("#password", "password");
+  await page.setViewport({
+    width: 1440,
+    height: 800
+  });
 
-  await delay(4000);
+  await page.goto("https://clickmeeting.com/free-signup", { waitUntil: "networkidle0" });
 
-  await page.$eval("#submit-button", elem => elem.click());
+  await page.type("#user_name", companyName);
+  await page.type("#user_email", login);
+  await page.type("#user_password", password);
+
+  await delay(2000);
+
+  await page.$eval(".check-element", elem => elem.click());
+
+  await delay(3000);
+
+  await page.$eval(".btn-signup-free", elem => elem.click());
 
   await page.waitForNavigation();
 
-  console.log("New Page URL:", page.url());
-
-  await page.screenshot({ path: "example.png" });
+  if (page.url() === "https://account-panel.clickmeeting.com/verify-account") {
+    console.log("Registration successfull");
+    console.log("User login: " + login);
+    console.log("User password: " + password);
+  }
 
   await browser.close();
 })();
