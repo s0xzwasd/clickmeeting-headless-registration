cf58ef8fc6d08ca7f3d439b00f55ad16c44964c3
Daniil Maslov <s0xzwasd@protonmail.com>
2020-03-21 18:03:13 +0300

    fix: code refactor

--- registration.js
+++ registration.js
@@ -1,6 +1,6 @@
-const fs = require("fs-extra");
-const generator = require("generate-password");
-const puppeteer = require("puppeteer");
+const fs = require(`fs-extra`);
+const generator = require(`generate-password`);
+const puppeteer = require(`puppeteer`);
 
 const args = process.argv.slice(2);
 const accountID = args[0];
@@ -8,60 +8,49 @@ const companyName = args[1];
 const customEmail = args[2];
 
 const addNewData = (login, password) => {
-  fs.writeFile("users.txt", login + ";" + password + "\n", { flag: "a+" }, err => {
-    console.log(err ? err : "No errors! :)");
-  });
-};
-
-const delay = time => {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, time);
+  fs.writeFile(`users.txt`, `${login};${password}\n`, { flag: `a+` }, error => {
+    console.log(error ? error : `Values ​​were successfully saved to file!`);
   });
 };
 
 (async () => {
   const browser = await puppeteer.launch();
+  const login = customEmail ? customEmail : `tb-master+${accountID}@ya.ru`;
   const page = await browser.newPage();
-  const registrationPage = "https://clickmeeting.com/free-signup";
-  const successPage = "https://account-panel.clickmeeting.com/verify-account";
-  const login = customEmail ? customEmail : "tb-master+" + accountID + "@ya.ru";
   const password = generator.generate({
     length: 10,
     numbers: true
   });
-
-  if (isNaN(accountID)) {
-    console.log("Please use a number for ACCOUNT_ID");
-    await browser.close();
-    return process.exit(1);
-  }
+  const registrationPage = `https://clickmeeting.com/free-signup`;
+  const successPage = `https://account-panel.clickmeeting.com/verify-account`;
 
   await page.setViewport({
     width: 1440,
     height: 800
   });
 
-  await page.goto(registrationPage, { waitUntil: "networkidle0" });
+  if (isNaN(accountID)) {
+    console.log(`Please use a numbers for ACCOUNT_ID. The previous value was: ${accountID}`);
+    await browser.close();
+    return process.exit(1);
+  }
 
-  await page.type("#user_name", companyName);
-  await page.type("#user_email", login);
-  await page.type("#user_password", password);
+  await page.goto(registrationPage, { waitUntil: `networkidle0` });
 
-  await delay(2000);
-
-  await page.$eval(".check-element", elem => elem.click());
-
-  await delay(3000);
-
-  await page.$eval(".btn-signup-free", elem => elem.click());
+  await page.type(`#user_name`, companyName);
+  await page.type(`#user_email`, login);
+  await page.type(`#user_password`, password);
+  await page.$eval(`.check-element`, elem => elem.click());
+  await page.$eval(`.btn-signup-free`, elem => elem.click());
 
   await page.waitForNavigation();
 
   if (page.url() === successPage) {
-    console.log("Registration successfull");
-    console.log("User login: " + login);
-    console.log("User password: " + password);
-
+    console.log(`
+      Registration successfull!
+      User login: ${login}
+      User password: ${password}
+    `);
     addNewData(login, password);
   }
 
